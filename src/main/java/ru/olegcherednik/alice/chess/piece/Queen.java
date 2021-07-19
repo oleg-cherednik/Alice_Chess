371723e8a8a6a94b8d0538767f40e5645fe1fee3
Oleg Cherednik <oleg.cherednik@gmail.com>
2021-07-20 02:19:28 +0300

    impl

--- src/main/java/ru/olegcherednik/alice/chess/piece/Queen.java
+++ src/main/java/ru/olegcherednik/alice/chess/piece/Queen.java
@@ -1,6 +1,10 @@
 package ru.olegcherednik.alice.chess.piece;
 
+import ru.olegcherednik.alice.chess.GameContext;
 import ru.olegcherednik.alice.chess.player.Player;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Oleg Cherednik
@@ -10,6 +14,20 @@ final class Queen extends AbstractPiece {
 
     public Queen(PieceId id, Player.Color color) {
         super(id, color);
+    }
+
+    @Override
+    public Set<String> getAvailableMoves(GameContext context) {
+        Set<String> cellIds = new HashSet<>();
+        addMultiMove(cellIds, -1, 0, context);
+        addMultiMove(cellIds, 1, 0, context);
+        addMultiMove(cellIds, 0, -1, context);
+        addMultiMove(cellIds, 0, 1, context);
+        addMultiMove(cellIds, -1, -1, context);
+        addMultiMove(cellIds, -1, 1, context);
+        addMultiMove(cellIds, 1, -1, context);
+        addMultiMove(cellIds, 1, 1, context);
+        return cellIds;
     }
 
 }
